5f1c6d9d6cc72f37dbce31d2cdcceeb11e6f9503
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-01-21 16:28:49 -0500

    dark mode update

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const closeButton = document.querySelector(".modal__content__close");
 const darkButton = document.querySelector(".dark-mode-button");
 const navbar = document.querySelector(".navbar");
 const loader = document.querySelector(".loader");
+const modalStyle = document.querySelector(".modal__content");
 
 button1.addEventListener("click", () => {
   modal.classList.remove("hidden");
@@ -24,6 +25,7 @@ darkButton.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   navbar.classList.toggle("dark-nav");
   loader.classList.toggle("dark");
+  modalStyle.classList.toggle("dark");
   if (document.body.classList.contains("dark")) {
     localStorage.setItem("mode", "dark");
   } else {
@@ -36,10 +38,12 @@ const getMode = () => {
     document.body.classList.add("dark");
     navbar.classList.add("dark-nav");
     loader.classList.add("dark");
+    modalStyle.classList.add("dark");
   } else {
     document.body.classList.remove("dark");
     navbar.classList.remove("dark-nav");
     loader.classList.remove("dark");
+    modalStyle.classList.remove("dark");
   }
 };
 
